a17859ad2944daa154279a9916f51bc9de670508
Michael Berezin <michaelbe@payoneer.com>
2023-05-18 09:59:40 +0300

    refactor

--- FlowContext.Api/Middlewares/ContextMiddleware.cs
+++ FlowContext.Api/Middlewares/ContextMiddleware.cs
@@ -21,16 +21,11 @@ public class ContextMiddleware
         //Adds a delegate to be invoked just before response headers will be sent to the client.
         context.Response.OnStarting(state =>
         {
-            //var (contextFlowId, contextParentId, contextSpanId) = Flow.GetContext();
-            //context.Response.Headers.Add(Flow.FlowIdName, contextFlowId);
-            //context.Response.Headers.Add(Flow.ParentIdName, contextParentId);
-            //context.Response.Headers.Add(Flow.SpanIdName, contextSpanId);
+            foreach (var item in Flow.GetContextAsDictionary())
+            {
+                context.Response.Headers.Add(item.Key, item.Value);
+            }
 
-             foreach (var item in Flow.GetContextAsDictionary())
-             {
-                 context.Response.Headers.Add(item.Key, item.Value);
-             }
-         
             return Task.CompletedTask;
         }, context);
 
